0ce6a2dab3f70069bef939f9cc2a8ca01f3fdc9a
kennethdeoperilla <82525450+kennethdeoperilla@users.noreply.github.com>
2021-04-19 12:30:42 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String Arg[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Value of x:");
+		int x = scanner.nextInt();
+		System.out.println("Value of y:");
+		int y = scanner.nextInt();
+		
+		
+		System.out.println("The value of x is " + x);
+		System.out.println("The value of y is " + y);
+		System.out.println("The value of x + y is " + (x+y));
+		
+		
+		
+	}
+
+}
